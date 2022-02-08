9a7f0f25019ece719b1abf14b05892785d3717b0
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-02-08 11:20:20 +0100

    Started working on scene 2

--- Assets/Scripts/Elias Bernmarker/Bomb/Bomb.cs
+++ Assets/Scripts/Elias Bernmarker/Bomb/Bomb.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    private ParticleSystem.EmissionModule em;
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<ParticleSystem>().Play();
+        em = GetComponent<ParticleSystem>().emission;
         StartCoroutine(BombStart());
     }
     IEnumerator BombStart()
     {
         yield return new WaitForSeconds(2);
         transform.GetChild(0).gameObject.SetActive(true);
+        em.enabled = true;
         yield return new WaitForSeconds(0.2f);
         Destroy(gameObject);
     }
